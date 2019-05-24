4b45a4f2d5437fbc8262ccb5cea50db4a17f7f98
Yusef Karim <yusefkarim@riseup.net>
2019-05-23 22:30:13 -0400

    Added really horrible polling to InputPin

--- learning-beaglebone/gpio_rust/src/lib.rs
+++ learning-beaglebone/gpio_rust/src/lib.rs
@@ -1,19 +1,20 @@
-use std::fs;
-use std::fs::{File, OpenOptions};
-use std::io::prelude::*;
+use std::fs::{self, File, OpenOptions};
+use std::io::{prelude::*, SeekFrom};
 use std::{thread, time};
 
 static GPIO_PATH: &str = "/sys/class/gpio/";
 
+#[derive(Debug)]
+enum Value { UNDEFINED = -1, LOW = 0, HIGH = 1 }
+enum ValueAsASCII { LOW = 48, HIGH = 49 }
 enum Direction { INPUT, OUTPUT }
-enum Value { UNDEFINED = -1, LOW = 48, HIGH = 49 }
 
 pub struct InputPin {
     pin_num: u32,
     full_path: String,
     buffer: Option<File>,
-    debounce_time: u32,
     last_value: Value,
+    debounce_time: u32,
 }
 
 pub struct OutputPin {
@@ -55,7 +56,8 @@ trait GPIO {
     fn read_value(value_file: &mut File) -> Value {
         let mut one_byte: [u8; 1] = [0; 1];
         value_file.read(&mut one_byte).expect("Unable to read value file");
-        if one_byte[0] == Value::LOW as u8 {
+        value_file.seek(SeekFrom::Start(0)).expect("Could not seek to start");
+        if one_byte[0] == ValueAsASCII::LOW as u8 {
             Value::LOW
         } else {
             Value::HIGH
@@ -63,14 +65,8 @@ trait GPIO {
     }
 }
 
-impl GPIO for InputPin {}
 impl GPIO for OutputPin {}
-
-impl Drop for InputPin {
-    fn drop(&mut self) {
-        Self::unexport(self.pin_num, &self.full_path);
-    }
-}
+impl GPIO for InputPin {}
 
 impl Drop for OutputPin {
     fn drop(&mut self) {
@@ -78,24 +74,12 @@ impl Drop for OutputPin {
     }
 }
 
-impl InputPin {
-    pub fn new(pin_num: u32) -> InputPin {
-        let full_path: String = format!("{}gpio{}/", GPIO_PATH, pin_num);
-        Self::export(pin_num, &full_path);
-        Self::set_direction(&full_path, Direction::INPUT);
-        let value_file_path: String = format!("{}value", &full_path);
-
-        InputPin {
-            pin_num: pin_num,
-            full_path: full_path,
-            buffer: Some(OpenOptions::new().read(true)
-                                           .open(value_file_path)
-                                           .unwrap()),
-            last_value: Value::UNDEFINED,
-            debounce_time: 0,
-        }
+impl Drop for InputPin {
+    fn drop(&mut self) {
+        Self::unexport(self.pin_num, &self.full_path);
     }
 }
+
 
 impl OutputPin {
     pub fn new(pin_num: u32) -> OutputPin {
@@ -117,13 +101,13 @@ impl OutputPin {
     }
 
     pub fn turn_on(&mut self) {
-        self.buffer.as_ref().unwrap().write(&[Value::HIGH as u8])
+        self.buffer.as_ref().unwrap().write(&[ValueAsASCII::HIGH as u8])
             .expect("Error writing value");
         self.last_value = Value::HIGH;
     }
 
     pub fn turn_off(&mut self) {
-        self.buffer.as_ref().unwrap().write(&[Value::LOW as u8])
+        self.buffer.as_ref().unwrap().write(&[ValueAsASCII::LOW as u8])
             .expect("Error writing value");
         self.last_value = Value::LOW;
     }
@@ -142,3 +126,34 @@ impl OutputPin {
     }
 }
 
+impl InputPin {
+    pub fn new(pin_num: u32) -> InputPin {
+        let full_path: String = format!("{}gpio{}/", GPIO_PATH, pin_num);
+        Self::export(pin_num, &full_path);
+        Self::set_direction(&full_path, Direction::INPUT);
+        let value_file_path: String = format!("{}value", &full_path);
+
+        InputPin {
+            pin_num: pin_num,
+            full_path: full_path,
+            buffer: Some(OpenOptions::new().read(true)
+                                           .open(value_file_path)
+                                           .unwrap()),
+            last_value: Value::UNDEFINED,
+            debounce_time: 0,
+        }
+    }
+
+    // NOTE: VERY INEFFICIENT, SHOULD BE CHANGE TO USE libc epoll INSTEAD
+    pub fn poll_until_high(&mut self) {
+        if let Value::UNDEFINED = self.last_value {
+            self.last_value = Self::read_value(self.buffer.as_mut().unwrap());
+        }
+
+        let ten_millis = time::Duration::from_millis(10);
+        while let Value::LOW = self.last_value {
+            self.last_value = Self::read_value(self.buffer.as_mut().unwrap());
+            thread::sleep(ten_millis);
+        }
+    }
+}
--- learning-beaglebone/gpio_rust/src/main.rs
+++ learning-beaglebone/gpio_rust/src/main.rs
@@ -3,12 +3,18 @@ use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, Ordering};
 use simple_signal::Signal;
 // From this module
-use gpio_rust::OutputPin;
+use gpio_rust::{OutputPin, InputPin};
 
 fn main() {
     let stay_alive = Arc::new(AtomicBool::new(true));
     let set_stay_alive = stay_alive.clone();
     let mut output: OutputPin = OutputPin::new(20);
+    let mut input: InputPin = InputPin::new(115);
+
+    simple_signal::set_handler(&[Signal::Int, Signal::Term], move |signals| {
+        println!("Received signal: {:?}\nExiting...", signals);
+        set_stay_alive.store(false, Ordering::SeqCst);
+    });
 
     let half_sec = time::Duration::from_millis(500);
 
@@ -20,10 +26,9 @@ fn main() {
         }
     });
 
-    simple_signal::set_handler(&[Signal::Int, Signal::Term], move |signals| {
-        println!("Received signal: {:?}\nExiting...", signals);
-        set_stay_alive.store(false, Ordering::SeqCst);
-    });
+    println!("Main thread is waiting for input pin to go HIGH");
+    input.poll_until_high();
+    println!("Input pin went high, joining child and waiting for you to exit.");
 
     child.join().expect("Could not join child thread");
 }
